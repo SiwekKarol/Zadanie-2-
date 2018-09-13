1aa29508b4319e994c39f1c8ee72f92b11341deb
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-13 21:51:14 +0200

    Ruszone zadanie 1.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-                Furniture furniture1 = new Furniture();
-
-        furniture1.nameFurniture = "Biurko";
-        furniture1.typeFurniture = "Mebel";
-        furniture1.age = 4.5;
-        furniture1.cityFurniture = "Wrocław";
+                Furniture furniture1 = new Furniture("Biurko", "Mebel", 4.5, "Wrocław");
 
         System.out.println(furniture1.nameFurniture);
         System.out.println(furniture1.cityFurniture);
         System.out.println(furniture1.age);
         System.out.println(furniture1.typeFurniture);
 
-        Furniture furniture2 = new Furniture();
-
-        furniture2.nameFurniture = "Szafa";
-        furniture2.typeFurniture = "Mebel";
-        furniture2.age = 3;
-        furniture2.cityFurniture = "Dobrzykowice";
-
+        Furniture furniture2 = new Furniture("Szafa","Mebel", 3, "Dobrzykowice");
+        
         System.out.println(furniture2.nameFurniture);
         System.out.println(furniture2.cityFurniture);
         System.out.println(furniture2.age);
