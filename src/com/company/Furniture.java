b995b156524b627550ff064b6f1dcbb92c443338
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-13 21:44:55 +0200

    Ruszone zadanie 1.2

--- src/com/company/Furniture.java
+++ src/com/company/Furniture.java
@@ -6,6 +6,14 @@ public class Furniture {
     double age;
     String cityFurniture;
 
+    Furniture (String nF, String tF, double a, String cF) {
+
+        nameFurniture = nF;
+        typeFurniture = tF;
+        age = a;
+        cityFurniture = cF;
+    }
+
     public Furniture() {
 
     }
