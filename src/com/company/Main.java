e75b6a181587979ce09528a28e42c7cb0ccc5d29
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-13 22:25:03 +0200

    Użyto funkcji printf zamiast println

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,25 @@ public class Main {
 
                 Furniture furniture1 = new Furniture("Biurko", "Mebel", 4.5, "Wrocław");
 
-        System.out.println(furniture1.nameFurniture);
-        System.out.println(furniture1.cityFurniture);
+        System.out.printf(furniture1.nameFurniture);
+        System.out.printf("%n");
+        System.out.printf(furniture1.cityFurniture);
+        System.out.printf("%n");
         System.out.println(furniture1.age);
-        System.out.println(furniture1.typeFurniture);
+        System.out.printf("%n");
+        System.out.printf(furniture1.typeFurniture);
+        System.out.printf("%n");
 
         Furniture furniture2 = new Furniture("Szafa","Mebel", 3, "Dobrzykowice");
-        
-        System.out.println(furniture2.nameFurniture);
-        System.out.println(furniture2.cityFurniture);
+
+        System.out.printf(furniture2.nameFurniture);
+        System.out.printf("%n");
+        System.out.printf(furniture2.cityFurniture);
+        System.out.printf("%n");
         System.out.println(furniture2.age);
-        System.out.println(furniture2.typeFurniture);{
+        System.out.printf("%n");
+        System.out.printf(furniture2.typeFurniture);
+        System.out.printf("%n");{
                 }
 
     }
